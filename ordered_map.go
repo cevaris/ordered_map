aeef4374b7ead9809660e2f2f7624173c4f501cd
Adam Cardenas <acardenas@twitter.com>
2016-05-07 15:11:42 -0600

    reorganization of code

--- ordered_map.go
+++ ordered_map.go
@@ -1,7 +1,6 @@
 package ordered_map
 
 import (
-	"bytes"
 	"fmt"
 )
 
@@ -111,71 +110,3 @@ func (om *OrderedMap) Len() int {
 	return len(om.store)
 }
 
-type node struct {
-	Prev  *node
-	Next  *node
-	Value interface{}
-}
-
-func newRootNode() *node {
-	root := &node{}
-	root.Prev = root
-	root.Next = root
-	return root
-}
-
-func newNode(prev *node, next *node, key interface{}) *node {
-	return &node{Prev: prev, Next: next, Value: key}
-}
-
-func (n *node) add(value string) {
-	root := n
-	last := root.Prev
-	last.Next = newNode(last, n, value)
-	root.Prev = last.Next
-}
-
-func (n *node) String() string {
-	var buffer bytes.Buffer
-	if n.Value == "" {
-		// Need to sentinel
-		var curr *node
-		root := n
-		curr = root.Next
-		for curr != root {
-			buffer.WriteString(fmt.Sprintf("%s, ", curr.Value))
-			curr = curr.Next
-		}
-	} else {
-		// Else, print pointer value
-		buffer.WriteString(fmt.Sprintf("%p, ", &n))
-	}
-	return fmt.Sprintf("LinkList[%v]", buffer.String())
-}
-
-func (n *node) IterFunc() func() (string, bool) {
-	var curr *node
-	root := n
-	curr = root.Next
-	return func() (string, bool) {
-		for curr != root {
-			tmp := curr.Value.(string)
-			curr = curr.Next
-			return tmp, true
-		}
-		return "", false
-	}
-}
-
-type KVPair struct {
-	Key   interface{}
-	Value interface{}
-}
-
-func (k *KVPair) String() string {
-	return fmt.Sprintf("%v:%v", k.Key, k.Value)
-}
-
-func (kv1 *KVPair) Compare(kv2 *KVPair) bool {
-	return kv1.Key == kv2.Key && kv1.Value == kv2.Value
-}
